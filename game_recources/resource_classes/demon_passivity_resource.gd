e3b567893d8c5c78afbcaee2200d1c01ed8b54cd
Ivan <malyshev.ivan@gmail.com>
2023-08-16 11:48:49 +0200

    Demon Passivity recovered

--- game_recources/resource_classes/demon_passivity_resource.gd
+++ game_recources/resource_classes/demon_passivity_resource.gd
@@ -1,6 +1,15 @@
 class_name DemonPassivityResource
 extends Resource
 
+enum Types {
+	NONE = 0,
+	HP_BUFF = 11, HP_REGEN = 12,
+	STAMINA_BUFF = 21, STAMINA_REGEN = 22, GRAVITY_BUFF = 23,
+	SPEED_BUFF = 31, SPEED_ACC_BUFF = 32,
+}
+@export var type: Types = Types.NONE
+@export_range(0, 100, 0.1) var value: float
+
 
 func _init() -> void:
-	super()
+	assert(type != Types.NONE, "Type is not set!")
--- src/gameplay/player_stuff/demon_passivity.gd
+++ src/gameplay/player_stuff/demon_passivity.gd
@@ -1,18 +1,8 @@
 class_name DemonPassivity
 extends Object
 
-var _type: DemonPassivityResource.Types
-var _value: float
+var resource: DemonPassivityResource
 
 
-func _init(type: DemonPassivityResource.Types, value: float) -> void:
-	_type = type
-	_value = value
-
-
-func get_type() -> DemonPassivityResource.Types:
-	return _type
-
-
-func get_value() -> int:
-	return _value
+func _init(resource: DemonPassivityResource) -> void:
+	self.resource = resource
